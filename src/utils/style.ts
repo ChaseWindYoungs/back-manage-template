20dc41d3aac94b19a0e4445c7e06bff540f059ec
chasewindyoungs <chasewindyoungs@163.com>
2024-01-08 17:48:52 +0800

    feat: 修改样式函数

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,27 +1,21 @@
 
 // 设置主题
-export function changeThemeValue(val?: any) {
-  const classes = document.getElementsByTagName('html')[0].className;
-  let classArr = [];
-  if (classes) {
-    classArr = classes.split(' ');
-  }
-  let keys = ['root-theme-light'];
-  classArr = classArr.filter(i => !keys.includes(i));
-  classArr.push('root-theme-light');
+export function changeThemeValue(flag: boolean) {
+  let classArr = [...document.getElementsByTagName('html')[0].classList]
+  flag ? (classArr = classArr.filter(i => i !== 'dark')) : classArr.push('dark')
   document.getElementsByTagName('html')[0].className = classArr.join(' ');
 };
 
 // 根据状态，初始化顶部距离
 export function initHeightFromTop(status, showTags, showAppTopBar) {
-  if(!status) {
+  if (!status) {
     document.documentElement.style.setProperty('--height-from-top', '0px');
   } else {
-    if(showTags && showAppTopBar) {
+    if (showTags && showAppTopBar) {
       document.documentElement.style.setProperty('--height-from-top', '100px');
-    } else if(showTags && !showAppTopBar){
+    } else if (showTags && !showAppTopBar) {
       document.documentElement.style.setProperty('--height-from-top', '40px');
-    } else if(!showTags && showAppTopBar){
+    } else if (!showTags && showAppTopBar) {
       document.documentElement.style.setProperty('--height-from-top', '60px');
     }
   }
