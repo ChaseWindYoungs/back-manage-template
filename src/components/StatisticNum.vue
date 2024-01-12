50785e2aced50277dbd70c3d03a762f9a4eb1bfd
chasewindyoungs <chasewindyoungs@163.com>
2024-01-12 17:18:26 +0800

    feat: 添加数字组件

---
+++ src/components/StatisticNum.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-statistic :value="Count" v-bind="$attrs" />
+</template>
+
+<script setup name="StatisticNum" lang="ts">
+/**
+ * StatisticNum
+ */
+import { ref, watch } from "vue";
+import { useTransition } from "@vueuse/core";
+
+interface Props {
+  value?: number;
+  duration?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: 0,
+  duration: 1500,
+});
+
+const num = ref(0);
+const Count = useTransition(num, {
+  duration: props.duration,
+});
+
+watch(
+  () => props.value,
+  () => (num.value = props.value),
+  { immediate: true }
+);
+</script>
+<style lang="scss" scoped></style>
