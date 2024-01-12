46d57c29d61a130e84e8e1e7315ae491eb617100
chasewindyoungs <chasewindyoungs@163.com>
2024-01-12 17:17:35 +0800

    feat: 添加函数式组件，动画组件

---
+++ src/components/Functional/Motion/index.ts
@@ -0,0 +1,44 @@
+import { h, defineComponent, withDirectives, resolveDirective } from "vue";
+
+/** 
+ * 利用渲染函数h
+ * 封装@vueuse/motion动画库中的自定义指令v-motion, 
+ * 生成函数式vnode组件，
+ * */
+export default defineComponent({
+  name: "Motion",
+  props: {
+    delay: {
+      type: Number,
+      default: 50
+    }
+  },
+  render() {
+    const { delay } = this;
+    const motion = resolveDirective("motion");
+    return withDirectives(
+      h(
+        "div",
+        {},
+        {
+          default: () => [this.$slots.default()]
+        }
+      ),
+      [
+        [
+          motion,
+          {
+            initial: { opacity: 0, y: 100 },
+            enter: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay
+              }
+            }
+          }
+        ]
+      ]
+    );
+  }
+});
